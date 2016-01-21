b5a19713fb343947b98044b51194ce95b1e29065
Duncan Law-Green <git@kyubi.co.uk>
2016-01-21 20:07:46 +0000

    Add flow routing

---
+++ client/routes.js
@@ -0,0 +1,22 @@
+"use strict"
+FlowRouter.route('/createItem', {
+    action: function(params) {
+	BlazeLayout.render("defaultLayout", {top: "defaultHeader", main: "createItem"});
+    }
+});
+FlowRouter.route('/itemList', {
+    action: function(params) {
+	BlazeLayout.render("defaultLayout", {top: "defaultHeader", main: "itemsList"});
+    }
+});
+FlowRouter.route('/updateItem', {
+    action: function(params) {
+	BlazeLayout.render("defaultLayout", {top: "defaultHeader", main: "updateItem"});
+    }
+});
+FlowRouter.route('/', {
+    action: function(params) {
+	BlazeLayout.render("defaultLayout", {top: "defaultHeader", main: "itemsList"});
+    }
+});
+
